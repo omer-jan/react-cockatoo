04a82378feb9d0fe2cdc5c16d388ca271e48fe59
Mohammad Zubair akrami <paghmane@yahoo.com>
2023-02-13 13:47:06 +0430

    Assignment lesson 1.5 is done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AddTodoForm from "./AddTodoForm";
 import TodoList from "./TodoList";
-
+const useSemiPersistentState =()=>{
+  // initialTodoList will get the stored list from localStorage if list dose not exist then it will be initialized with empty array 
+  const initialTodoList=JSON.parse(localStorage.getItem('savedTodoList'))==null?[]:JSON.parse(localStorage.getItem('savedTodoList'));
+  // State variable
+  const [todoList, setTodoList] = useState(initialTodoList)
+  // useEffect hook to store data in local storage 
+  useEffect(() => { 
+   localStorage.setItem('savedTodoList',JSON.stringify(todoList));
+  },[todoList]);
+  return [todoList,setTodoList];
+}
 function App() { 
-
-  const [todoList, setTodoList] = useState([]);
-
+  const[todoList,setTodoList]=useSemiPersistentState()
   let addTodo =(newTodo)=>{ 
-  //  console.log(newTodo);
     setTodoList([...todoList,newTodo])        
-  }
-  
+  }  
   return (
     <>
      <h1>Todo List</h1>
